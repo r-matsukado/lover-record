714771020389350cec0edea125bd2ff1bf311a6e
r-matsukado <matsukado.ryo.dev123@gmail.com>
2019-04-04 22:20:33 +0900

    first commit

---
+++ virtual-web-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matsukado/PycharmProjects/lover-record/virtual-web-env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
